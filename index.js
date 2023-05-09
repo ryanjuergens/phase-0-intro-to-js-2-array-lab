e0369f4fe5b103c257fa8e28d11610d3de287967
Ryan Juergens <rjuergens@hotmail.com>
2023-05-08 23:10:49 -0500

    passes tests

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"]
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(name){
+    const catsAppend = [...cats, `${name}`]
+    return catsAppend
+}
+function prependCat(name){
+    const catsPrepend = [`${name}`, ...cats]
+    return catsPrepend
+}
+function removeLastCat(){
+    return cats.slice(0, -1)
+} 
+function removeFirstCat(){
+    return cats.slice(1)
+}
